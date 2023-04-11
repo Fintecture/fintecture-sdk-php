76a2c5a15b5fe6ecf1df852a1a7628a9e845e879
Hugo Posnic <hugo@fintecture.com>
2023-04-11 15:20:30 +0200

    2.3.4

--- src/Fintecture.php
+++ src/Fintecture.php
@@ -15,7 +15,7 @@ use Psr\Http\Message\StreamFactoryInterface;
 final class Fintecture
 {
     // SDK Version
-    public const VERSION = '2.3.3';
+    public const VERSION = '2.3.4';
 
     // API URLs
 
